0d0939be1d8abfb59ecec32c803a2cbbc67bdab7
Thomas Beckmann <thomas.beckmann@mway.io>
2021-10-05 13:02:41 +0200

    sample using client

---
+++ sample/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.logging.Logger;
+
+public class Main {
+    private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(final String[] args) {
+        LOGGER.info("Hello from " + Main.class.getName());
+    }
+
+}
